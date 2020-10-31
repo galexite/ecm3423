5438f6c6a921b2219c0712f7b11758bb11e95445
George White <me@galexite.uk>
2020-10-31 10:35:11 +0000

    Fix surface normals

--- ecm3423/mesh.py
+++ ecm3423/mesh.py
@@ -23,19 +23,20 @@ class Mesh:
 
         for i, f in enumerate(self.faces):
             u = self.vertices[f[1]] - self.vertices[f[0]]
-            v = self.vertices[f[3]] - self.vertices[f[0]]
+            v = self.vertices[f[2]] - self.vertices[f[0]]
 
-            self.normals[i, 0] = (u[1] * v[2]) - (u[2] * v[1])
-            self.normals[i, 1] = (u[2] * v[0]) - (u[0] * v[2])
-            self.normals[i, 2] = (u[0] * v[1]) - (u[1] - v[0])
+            normal = np.cross(u, v)
+            self.normals[f, :] += normal / np.linalg.norm(normal)
+        
+        self.normals /= np.linalg.norm(self.normals, axis=1, keepdims=True)
 
     @staticmethod
-    def from_obj_file(path: str) -> Mesh:
+    def from_obj_file(path: str) -> "Mesh":
         vertices = []
         faces = []
 
         with open(path, "r") as fp:
-            for line, num in enumerate(fp):
+            for num, line in enumerate(fp):
                 spl = line.split()
 
                 if spl[0] == "#":
@@ -46,7 +47,7 @@ class Mesh:
                             f"{path}, line {num}: v must by accompanied by 3 float components"
                         )
 
-                    vertices.append(map(float, spl[1:]))
+                    vertices.append(spl[1:]) # numpy casts to float for us later
                     continue
                 elif spl[0] == "f":
                     if len(spl) != 4 and len(spl) != 5:
@@ -55,7 +56,7 @@ class Mesh:
                         )
 
                     faces.append(
-                        [[np.uint32(i) - 1 for j in i.split()] for i in spl[1:]]
+                        [[np.uint32(j) - 1 for j in i.split("/")][0] for i in spl[1:]]
                     )
                     continue
 
