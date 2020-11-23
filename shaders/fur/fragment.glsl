3c5c4a771e0e7b541749a815abb73d4279d935fa
George White <me@galexite.uk>
2020-11-23 14:06:42 +0000

    Avoid shader branching

--- shaders/fur/fragment.glsl
+++ shaders/fur/fragment.glsl
@@ -17,7 +17,8 @@ void main()
     // Sample the fur texture. Only the red component will be meaningful, and this will be used as the fur's alpha.
     vec4 sample = texture(noise_texture, fs_normal.xy / density);
 
-    // Draw the fur based
+    // Draw the fur.
+    float base_of_fur = step(fs_layer, 0.0); // 1.0 if this vertex is at the fur's base, avoids branching in the GPU
     frag_color = vec4(smoothstep(0.2, 1.0, fs_layer) * fs_color,
-        fs_layer == 0.0 ? 1.0 : sample.r * (1 - fs_layer));
+        base_of_fur + (1 - base_of_fur) * sample.r * (1 - fs_layer));
 }
